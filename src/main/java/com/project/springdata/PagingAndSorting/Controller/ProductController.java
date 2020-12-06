cebe7586c89c9e37d14023d53712da8d84725e21
mertcakmak <mertcakmak2@gmail.com>
2020-12-06 12:26:29 +0300

    Dinamik olarak gelen alanlara gore sortlama ve jUnitTest

--- src/main/java/com/project/springdata/PagingAndSorting/Controller/ProductController.java
+++ src/main/java/com/project/springdata/PagingAndSorting/Controller/ProductController.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,10 +23,25 @@ public class ProductController {
 	@Autowired
 	public IProductRepository repository;
 	
-	@GetMapping("/pagination/{page}/{size}")
+	@GetMapping("/products")
+	public List<Product> getProducts() {
+		return (List<Product>) repository.findAll();
+	}
+	
+	@GetMapping("/pagination/{page}/{size}") //Pagination(name alanına göre sıralayarak)
 	public List<Product> getPageable(@PathVariable int page, @PathVariable int size ) {
 //		Pageable firstPageWithTwoElements = PageRequest.of(page, size) ;		
 		Pageable firstPageWithTwoElements = PageRequest.of(page, size, Sort.by("name"));
 		return repository.findAll(firstPageWithTwoElements).getContent(); 
+	}
+	
+	@GetMapping("/sort") //Birden cok alana gore siralama.
+	public List<Product> getPageableMultiField() {
+		return (List<Product>) repository.findAll(Sort.by("name","price"));
+	}
+	
+	@PostMapping("/sortMultiFields") //Dinamik olarak gönderilen alanlara gore siralama.
+	public List<Product> getPageableMultiField(@RequestBody String [] fields) {
+		return (List<Product>) repository.findAll(Sort.by(fields));
 	}
 }
--- src/test/java/com/project/springdata/PagingAndSorting/PagingAndSortingApplicationTests.java
+++ src/test/java/com/project/springdata/PagingAndSorting/PagingAndSortingApplicationTests.java
@@ -1,13 +1,37 @@
 package com.project.springdata.PagingAndSorting;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+
+import com.project.springdata.PagingAndSorting.Entities.Product;
+import com.project.springdata.PagingAndSorting.Repos.IProductRepository;
 
 @SpringBootTest
 class PagingAndSortingApplicationTests {
+	
+	@Autowired
+	IProductRepository repository;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testMultiSort() {
+//		List<Product> products = (List<Product>) repository.findAll(Sort.by("name").and(Sort.by("price")));
+		List<Product> products =  (List<Product>) repository.findAll(Sort.by("name","price"));
+		products.forEach(p -> System.out.println(p.getName()+" "+p.getPrice()));
+	}
+	
+	@Test
+	public void testMultiSortByList() {
+		String [] fields = {"name","price"};
+		List<Product> products =  (List<Product>) repository.findAll(Sort.by(fields));
+		products.forEach(p -> System.out.println(p.getName()+" "+p.getPrice()));
 	}
 
 }
